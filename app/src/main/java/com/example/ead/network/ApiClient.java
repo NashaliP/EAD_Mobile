8e45a5a16cf43ac991b213b3e53565a090067e68
Nashali Perera <nashaliperera0@gmail.com>
2024-09-25 02:04:42 +0530

    fix: change api connection

--- app/src/main/java/com/example/ead/network/ApiClient.java
+++ app/src/main/java/com/example/ead/network/ApiClient.java
@@ -1,7 +1,10 @@
 package com.example.ead.network;
 
-import retrofit2.Retrofit;
-import retrofit2.converter.gson.GsonConverterFactory;
+        import okhttp3.OkHttpClient;
+        import okhttp3.logging.HttpLoggingInterceptor;
+        import retrofit2.Retrofit;
+        import retrofit2.converter.gson.GsonConverterFactory;
+
 
 public class ApiClient {
     private static final String BASE_URL = "http://10.0.2.2:15240/api/";
@@ -9,9 +12,17 @@ public class ApiClient {
 
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
+            HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor();
+            loggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+
+            OkHttpClient client = new OkHttpClient.Builder()
+                    .addInterceptor(loggingInterceptor)
+                    .build();
+
             retrofit = new Retrofit.Builder()
                     .baseUrl(BASE_URL)
                     .addConverterFactory(GsonConverterFactory.create())
+                    .client(client)
                     .build();
         }
         return retrofit;
